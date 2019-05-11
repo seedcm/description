3f0a65f12fac5e4bfa15b2c4f00cb2c133f79b4c
seedcm <seedcm@163.com>
2019-05-11 09:46:41 +0800

    update

--- arg_add.c
+++ arg_add.c
@@ -1,10 +1,21 @@
 // 带参数加法运算
+// 检测是否带参数
+// 检测参数是否满足可供计算的最小参数数量（2）
+// 检测参数是否为数字（int、float）
+// 返回运算结果
 
 #include <stdio.h>
 
+/* 创建带参数主函数 */
 int main(int argc, char** argv)
 {
-    int num, sum, n;
+    /* 创建变量 */
+    int int_num;        // 整型变量
+    int int_sum;        // 整型数值合计
+    int float_num;      // 浮点型变量
+    int float_sum;      // 浮点型数值合计
+    int double_num;     // 
+    int double_sum;
 
     if (argc <= 2)
     {
